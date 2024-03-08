014455aeaa1371c8d807168d2a817641fee6210e
zhangweidong <zhangwdong8@163.com>
2024-03-08 14:57:49 +0800

    自动化部署

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'dumi';
+const repo = 'dumi-java-docs'; // 项目名
 
 export default defineConfig({
+  title: 'dumi-java-docs',
   themeConfig: {
     name: 'dumi docs',
   },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
