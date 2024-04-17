ce9bf03ac60fc78f1abf6b0bfc765db5298b5ec1
zhangweidong <weidong.zhang@jg-robust.com>
2024-04-17 10:44:34 +0800

    test

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'dumi';
-const repo = 'saas'; // 项目名
+const repo = 'dumi-java-docs'; // 项目名
 
 export default defineConfig({
   title: 'dumi-java-docs',
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
-  exportStatic: {},
-  logo: '/saas/MSH-logo.jpg',
+  // exportStatic: {},
+  logo: '/dumi-java-docs/MSH-logo.jpg',
   // favicon: '/public/saas/favicon.ico',
   themeConfig: {
     name: 'dumi docs',
